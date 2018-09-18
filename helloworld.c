5107a3b674daaca5ffa2a8b79b023f344bd2147b
Nikhil Thadeshwar <43295252+NIKHILTHADESHWAR@users.noreply.github.com>
2018-09-18 22:01:22 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include<stdio.h> //header file
+
+int main()  //Entry point for every program in C.
+{
+    printf("Hello World!"); //'printf' is a function used to display the arguments in it.
+    
+    return 0 ; //The return statement terminates the execution of a function and returns control to the calling funtion.
+}
